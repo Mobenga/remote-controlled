8779b260deb6475c223cc7471a1d19c2fefb5d52
Johan Frick <johan.frick@mobenga.com>
2016-11-29 11:03:24 +0100

    Websocket server: allow cross origin

--- websocket-server/src/main/java/hello/WebSocketConfig.java
+++ websocket-server/src/main/java/hello/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/todo-websocket").withSockJS();
+        registry.addEndpoint("/todo-websocket").setAllowedOrigins("*").withSockJS();
     }
 
 }
