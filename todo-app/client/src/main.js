e5f655caf1fa562e5aef7fe279e3c6f3964f5807
Albert Mojir <albert.mojir@mobenga.com>
2016-12-06 09:57:43 +0100

    Renamed remoteControl to remoteControlClient in todo app

--- todo-app/client/src/main.js
+++ todo-app/client/src/main.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import {connect} from "./remoteControl";
+import {connect} from "./remoteControlClient";
 
 import * as model from "./model";
 
